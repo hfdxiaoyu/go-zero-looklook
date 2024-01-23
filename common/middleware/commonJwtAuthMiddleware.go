564d4804a5e334acabb142b77d7340b4448af6a3
hfdxiaoyu <1306933520@qq.com>
2024-01-23 15:06:33 +0800

    新增代码注释

--- common/middleware/commonJwtAuthMiddleware.go
+++ common/middleware/commonJwtAuthMiddleware.go
@@ -1,21 +1,25 @@
 package middleware
 
 import (
-	"github.com/zeromicro/go-zero/rest/handler"
 	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/handler"
 )
 
 // CommonJwtAuthMiddleware : with jwt on the verification, no jwt on the verification
+// 是否启用jwt验证
 type CommonJwtAuthMiddleware struct {
 	secret string
 }
 
+// 函数“NewCommonJwtAuthMiddleware”返回带有提供的秘密的“CommonJwtAuthMiddleware”结构的新实例。
 func NewCommonJwtAuthMiddleware(secret string) *CommonJwtAuthMiddleware {
 	return &CommonJwtAuthMiddleware{
 		secret: secret,
 	}
 }
 
+// “Handle”函数是“CommonJwtAuthMiddleware”结构的一个方法。它采用“http.HandlerFunc”作为参数并返回“http.HandlerFunc”。
 func (m *CommonJwtAuthMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if len(r.Header.Get("Authorization")) > 0 {
--- common/uniqueid/uniqueid.go
+++ common/uniqueid/uniqueid.go
@@ -5,14 +5,16 @@ import (
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
+// 生成分布式的唯一id
 var flake *sonyflake.Sonyflake
 
 func init() {
+	// 实例化生成分布式id的方法
 	flake = sonyflake.NewSonyflake(sonyflake.Settings{})
 }
 
 func GenId() int64 {
-
+	// NextID生成下一个唯一ID。在Sonyflake时间溢出后，NextID返回一个错误。
 	id, err := flake.NextID()
 	if err != nil {
 		logx.Severef("flake NextID failed with %s \n", err)
